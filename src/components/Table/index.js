e8276974be5a491ad27c6be25d1b1701d69d6f87
Sarah Kerr <sarah.kerr93@gmail.com>
2020-10-09 20:17:05 -0400

    css for name sort buttons

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -25,35 +25,38 @@ class Table extends Component {
     }
   }
 
-  sortName = () => {
+  sortNameAZ = () => {
     let sortedList = [];
-    if (!this.state.alpha) {
-      sortedList = this.props.empList.sort((a, b) => {
-        var nameA = a.name.last.toLowerCase();
-        var nameB = b.name.last.toLowerCase();
-        if (nameA < nameB) return -1;
-        if (nameA > nameB) return 1;
-        return 0;
-      });
-      this.setState({
-        alpha: true,
-        revAlpha: false,
-        sortedEmployees: sortedList,
-      });
-    } else if (this.state.alpha) {
-      sortedList = this.props.empList.sort((a, b) => {
-        var nameA = a.name.last.toLowerCase();
-        var nameB = b.name.last.toLowerCase();
-        if (nameA > nameB) return -1;
-        if (nameA < nameB) return 1;
-        return 0;
-      });
-      this.setState({
-        alpha: false,
-        revAlpha: true,
-        sortedEmployees: sortedList,
-      });
-    }
+    // if (!this.state.alpha) {
+    sortedList = this.props.empList.sort((a, b) => {
+      var nameA = a.name.last.toLowerCase();
+      var nameB = b.name.last.toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    });
+    this.setState({
+      alpha: true,
+      revAlpha: false,
+      sortedEmployees: sortedList,
+    });
+  };
+
+  sortNameZA = () => {
+    let sortedList = [];
+    // if (!this.state.alpha) {
+    sortedList = this.props.empList.sort((a, b) => {
+      var nameA = a.name.last.toLowerCase();
+      var nameB = b.name.last.toLowerCase();
+      if (nameA > nameB) return -1;
+      if (nameA < nameB) return 1;
+      return 0;
+    });
+    this.setState({
+      alpha: false,
+      revAlpha: true,
+      sortedEmployees: sortedList,
+    });
   };
 
   render() {
@@ -61,8 +64,14 @@ class Table extends Component {
       <table>
         <thead>
           <th>Photo</th>
-          <th class="nameButton" onClick={this.sortName}>
+          <th class="nameButton">
+            <button class="arrowBtn btn btn-light" onClick={this.sortNameAZ}>
+              ↓
+            </button>
             Name
+            <button class="arrowBtn btn btn-light" onClick={this.sortNameZA}>
+              ↑
+            </button>
           </th>
           <th>Phone</th>
           <th>Gender</th>
